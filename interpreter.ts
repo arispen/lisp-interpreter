0955a4cff69f10c619a7edae982ca30507ce4a88
arispen <arispen@gmail.com>
2022-09-11 11:25:36 +0200

    eval wip

--- interpreter.ts
+++ interpreter.ts
@@ -1,10 +1,10 @@
 type LispSymbol = string
-type LispList = Array<LispAtom>
+type LispList = Array<LispList | LispAtom>
 type LispNumber = number
 
 type LispAtom = LispNumber | LispSymbol
 
-class Interpreter {
+class LispInterpreter {
 
     tokenize(program: string): string[] {
         return program.replace(/\(/g, " ( ").replace(/\)/g, " ) ").trim().split(/\s+/);
@@ -14,13 +14,13 @@ class Interpreter {
         let syntax: LispList | LispAtom = [];
         if (tokens.length === 0) {
             console.error("Unexpected EOF while reading");
-        } 
+        }
         const token = tokens.shift();
-        if (token === '('){
+        if (token === '(') {
             const expression: LispList = [];
             while (tokens[0] !== ')') {
                 expression.push(this.readFromTokens(tokens) as LispAtom);
-            }  
+            }
             tokens.shift();
             syntax = expression;
         }
@@ -35,15 +35,22 @@ class Interpreter {
     }
 
     parse(program: string) {
-        return this.readFromTokens(this.tokenize(program))
+        return this.readFromTokens(this.tokenize(program));
     }
-    
-    atom(token: unknown): LispAtom{
+
+    atom(token: unknown): LispAtom {
         const number = Number(token);
-        if(isNaN(number)) {
-            return token as LispSymbol
+        if (isNaN(number)) {
+            return token as LispSymbol;
         } else {
             return number;
+        }
+    }
+
+    eval(expression: LispList, environment = globalEnvironment) {
+        if(typeof expression === "string") {
+            const env = environment.findEnvironment(expression);
+            return env[expression];
         }
     }
 }
