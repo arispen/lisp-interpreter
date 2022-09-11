c95ed28b40cae7f53daff7bdb850e10e57778223
arispen <arispen@gmail.com>
2022-09-11 10:55:13 +0200

    interpreter wip

---
+++ interpreter.ts
@@ -0,0 +1,49 @@
+type LispSymbol = string
+type LispList = Array<LispAtom>
+type LispNumber = number
+
+type LispAtom = LispNumber | LispSymbol
+
+class Interpreter {
+
+    tokenize(program: string): string[] {
+        return program.replace(/\(/g, " ( ").replace(/\)/g, " ) ").trim().split(/\s+/);
+    }
+
+    readFromTokens(tokens: string[]): LispList | LispAtom {
+        let syntax: LispList | LispAtom = [];
+        if (tokens.length === 0) {
+            console.error("Unexpected EOF while reading");
+        } 
+        const token = tokens.shift();
+        if (token === '('){
+            const expression: LispList = [];
+            while (tokens[0] !== ')') {
+                expression.push(this.readFromTokens(tokens) as LispAtom);
+            }  
+            tokens.shift();
+            syntax = expression;
+        }
+        else {
+            if (token === ')') {
+                console.error("Unexpected )");
+            } else {
+                syntax = this.atom(token);
+            }
+        }
+        return syntax;
+    }
+
+    parse(program: string) {
+        return this.readFromTokens(this.tokenize(program))
+    }
+    
+    atom(token: unknown): LispAtom{
+        const number = Number(token);
+        if(isNaN(number)) {
+            return token as LispSymbol
+        } else {
+            return number;
+        }
+    }
+}
