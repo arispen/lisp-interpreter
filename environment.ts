a9451dd5619575791fc1e202c5ae29d1a53ed8ef
arispen <arispen@gmail.com>
2022-09-13 16:35:21 +0200

    add params and args to environment

--- environment.ts
+++ environment.ts
@@ -1,17 +1,31 @@
 class LispEnvironment extends Map {
-    public paramDefinitions: LispSymbol[];
-    public argsValues: LispAtom[];
     public outerEnvironment: LispEnvironment;
 
-    constructor(paramDefinitions?: LispSymbol[], 
-        argsValues?: LispAtom[], 
+    constructor(paramDefinitions?: LispSymbol[],
+        argsValues?: LispAtom[],
         outerEnvironment?: LispEnvironment) {
         super();
+        if (outerEnvironment) {
+            this.outerEnvironment = outerEnvironment;
+        }
         this.set("+", (a: number, b: number) => a + b);
-        // TODO: set params and args
+        if (paramDefinitions && paramDefinitions?.length > 0
+            && argsValues && argsValues.length === paramDefinitions.length) {
+            this.addArgsToEnv(paramDefinitions, argsValues)
+        }
     }
 
-    findEnvironment(variableName: LispSymbol): LispEnvironment {
+    private addArgsToEnv(
+        paramDefinitions: LispSymbol[],
+        argsValues: LispAtom[]) {
+        for (let index = 0; index < paramDefinitions.length; index++) {
+            const param = paramDefinitions[index];
+            const arg = argsValues[index];
+            this.set(param, arg);
+        }
+    }
+
+    public findEnvironment(variableName: LispSymbol): LispEnvironment {
         if (this.get(variableName) !== undefined) {
             return this
         } else {
